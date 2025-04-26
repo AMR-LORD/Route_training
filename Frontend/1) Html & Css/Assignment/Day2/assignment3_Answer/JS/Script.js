166e890d089b8912fa72eceaf57d50dc8d3a907b
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-04-26 15:53:13 +0300

    Add initial HTML structure and CSS file for Day 7 tasks

--- Frontend/1) Html & Css/Assignment/Day2/assignment3_Answer/JS/Script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-    let navbar = document.querySelector(".navbar");
-    
-    if (window.scrollY > 0) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
